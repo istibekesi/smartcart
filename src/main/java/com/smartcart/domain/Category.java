a35b646a19e9e2520f649ed84a25d98246fe3acd
istibekesi <istibekesi@gmail.com>
2015-07-16 18:06:30 +0200

    New entity: category

---
+++ src/main/java/com/smartcart/domain/Category.java
@@ -0,0 +1,110 @@
+package com.smartcart.domain;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+/**
+ * A Category.
+ */
+@Entity
+@Table(name = "CATEGORY")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@Document(indexName="category")
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "parent")
+    @JsonIgnore
+    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+    private Set<Category> children = new HashSet<>();
+
+    @ManyToOne
+    private Category parent;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Category> getChildren() {
+        return children;
+    }
+
+    public void setChildren(Set<Category> categorys) {
+        this.children = categorys;
+    }
+
+    public Category getParent() {
+        return parent;
+    }
+
+    public void setParent(Category category) {
+        this.parent = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Category category = (Category) o;
+
+        if ( ! Objects.equals(id, category.id)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + "'" +
+                '}';
+    }
+}
---
+++ src/main/webapp/scripts/app/entities/category/category.js
@@ -0,0 +1,47 @@
+'use strict';
+
+angular.module('smartcartApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('category', {
+                parent: 'entity',
+                url: '/category',
+                data: {
+                    roles: ['ROLE_USER'],
+                    pageTitle: 'smartcartApp.category.home.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/entities/category/categorys.html',
+                        controller: 'CategoryController'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('category');
+                        $translatePartialLoader.addPart('global');
+                        return $translate.refresh();
+                    }]
+                }
+            })
+            .state('categoryDetail', {
+                parent: 'entity',
+                url: '/category/:id',
+                data: {
+                    roles: ['ROLE_USER'],
+                    pageTitle: 'smartcartApp.category.detail.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/entities/category/category-detail.html',
+                        controller: 'CategoryDetailController'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('category');
+                        return $translate.refresh();
+                    }]
+                }
+            });
+    });
