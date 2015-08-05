5b474d927231b4feb6b150fb867875e25085bf81
Békési István <istibekesi@gmail.com>
2015-08-06 00:23:39 +0200

    map v3 api

--- src/main/webapp/smartcart/scripts/smartCartApp.js
+++ src/main/webapp/smartcart/scripts/smartCartApp.js
@@ -2,5 +2,31 @@ var app = angular.module('smartCartApp', ['ngResource']);
 
 app.controller('MainCtrl', function($scope, $resource) {
 
+	 $scope.shopData = {
+			 shops : [
+			          {
+			        	  brand : "TESCO",
+			        	  name : "TESCO Veszprém Hipermarket",
+			        	  address: "8200 Külső-Kádártai út",
+			        	  lat: 47.1065305,
+			        	  lng: 17.9269768
+			          },
+			          {
+			        	  brand : "TESCO",
+			        	  name : "TESCO Veszprém Szupermarket",
+			        	  address: "8200 Mártírok útja 13.",
+			        	  lat: 47.0843779,
+			        	  lng: 17.9134511
+			          },
+			          {
+			        	  brand : "INTERSPAR",
+			        	  name : "Interspar",
+			        	  address: "8200 Dornyai Béla u. 4",
+			        	  lat: 47.0843483,
+			        	  lng: 17.9129987
+			          }
+			          
+			 ]
+	 }
 	
 });
